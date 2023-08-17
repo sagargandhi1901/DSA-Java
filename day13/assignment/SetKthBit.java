94fffd01b2d3430ba9fe3f048d4b7c715108c859
Sagar Gandhi <sagargandhi968@gmail.com>
2023-08-17 19:09:50 +0530

    Day 13

---
+++ day13/assignment/SetKthBit.java
@@ -0,0 +1,16 @@
+package day13.assignment;
+
+import java.util.Scanner;
+
+public class SetKthBit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int value = 1 << k;
+        int answer = n | value;
+        System.out.println(answer);
+    }
+
+}
